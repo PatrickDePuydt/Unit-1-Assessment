87d094c994384154ad82c72c2a197e066e7a0582
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-07 11:00:09 -0700

    main: Get event handler speaking to DOM

--- js/main.js
+++ js/main.js
@@ -4,6 +4,13 @@ const increase = document.querySelector("[data-target='increase']");
 const input = document.querySelector("[data-target='input-text']");
 const decrease = document.querySelector("[data-target='decrease']");
 
+// Initializers
+//// Global Variables
+let finalNumber; // What we will finally display
+let initialNumber; // what number we are starting with
+//// Initiatives
+input.value = 100;
+
 
 // Functions
 
@@ -17,7 +24,12 @@ const handleDecrement = (event) => {
 };
 
 const handleKeyInput = (event) => {
-  console.log(`Decremented number: `, event.target.value);
+  initialNumber = Number(input.value);// Make sure the value is a number
+  // console.log(`Decremented number: `, inputNumber);
+  finalNumber = initialNumber;
+  counter.innerText = finalNumber;
+  console.log(`1. initialNumber: `, initialNumber);
+
 };
 
 //  Event Listeners
