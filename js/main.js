f2b891881976ec34612adb9407a7707a2efcc73b
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-07 10:30:48 -0700

    main: Add data selectors, sanity check

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const inputTracker = document.querySelector("[data-component='input-tracker']");
+const counter = document.querySelector("[data-target='counter']");
+const increase = document.querySelector("[data-target='increase']");
+const input = document.querySelector("[data-target='input-text']");
+const decrease = document.querySelector("[data-target='decrease']");
+
+console.log(`inputTracker: `, inputTracker);
+console.log(`counter: `, counter);
+console.log(`increase: `, increase);
+console.log(`input: `, input);
+console.log(`decrease: `, decrease);
