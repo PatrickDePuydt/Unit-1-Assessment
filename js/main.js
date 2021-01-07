c862e13540b061256bca20a0171b9e3e13e1c5e1
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-07 12:00:24 -0700

    main: Done>

--- js/main.js
+++ js/main.js
@@ -7,32 +7,37 @@ const decrease = document.querySelector("[data-target='decrease']");
 // Initializers
 //// Global Variables
 let finalNumber; // What we will finally display
+let globalNumber; // what number we are starting with
 let initialNumber; // what number we are starting with
+
 //// Initiatives
 input.value = 1;
 
 // Functions
-const updateDOM = (number) => counter.innerText = number;
+const updateDOM = (number) => {
+  counter.innerText = number;
+  globalNumber = number;
+}
 
 //// Event Handlers
 const handleIncrement = () => {
-  let increment = input.value++;
-  updateDOM(increment);
+  globalNumber = input.value++;
+  updateDOM(globalNumber);
 };
 
-const handleDecrement = (event) => {
-  let decrement = input.value--;
-  updateDOM(decrement);
+const handleDecrement = () => {
+  globalNumber = input.value--;
+  updateDOM(globalNumber);
 };
 
-// const handleKeyInput = (event) => {
-//   // console.log(`Decremented number: `, inputNumber);
-//   finalNumber = initialNumber;
-//   updateDOM(finalNumber);
-// };
+const handleKeyInput = (event) => {
+  let inputNumber = Number(event.target.value);
+  globalNumber = inputNumber;
+  updateDOM(globalNumber);
+};
 
 //  Event Listeners
 increase.addEventListener("click", handleIncrement);
 decrease.addEventListener("click", handleDecrement);
-// input.addEventListener("keyup", handleKeyInput);
+input.addEventListener("keyup", handleKeyInput);
 
