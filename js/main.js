10d3eb905b501070d37a6b78a9b1e15aa40f4fca
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-07 10:35:54 -0700

    main: add event listeners

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
-const inputTracker = document.querySelector("[data-component='input-tracker']");
+// Selectors
 const counter = document.querySelector("[data-target='counter']");
 const increase = document.querySelector("[data-target='increase']");
 const input = document.querySelector("[data-target='input-text']");
 const decrease = document.querySelector("[data-target='decrease']");
 
-console.log(`inputTracker: `, inputTracker);
-console.log(`counter: `, counter);
-console.log(`increase: `, increase);
-console.log(`input: `, input);
-console.log(`decrease: `, decrease);
+
+// Functions
+const increment = (event) => {
+  console.log(`Incremented number: `, event.target);
+};
+
+const decrement = (event) => {
+  console.log(`Decremented number: `, event.target);
+};
+
+
+//  Event Listeners
+increase.addEventListener("click", increment);
+decrease.addEventListener("click", decrement);
